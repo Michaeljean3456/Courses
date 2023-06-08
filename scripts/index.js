40d6008e3372ea638488060d297bcb84de1e9ada
Michael JeanBaptiste <michaeljean3456@gmail.com>
2023-06-08 18:56:37 -0400

    final

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ function getApi() {
   fetch("http://localhost:8081/api/courses/")
     .then(response => response.json())
     .then(info => {
-        let result = info.dept.courseNum.courseName;
+        let result = info;
       displayCourses(result);
     });
 }
@@ -20,12 +20,19 @@ function displayCourses(result) {
     let newRow = tableInput.insertRow(-1);
 
     let cell1 = newRow.insertCell(0);
-    cell1.textContent = course.courseName;
+    cell1.textContent = course.dept;
 
     let cell2 = newRow.insertCell(1);
-    cell2.textContent = course.startDate;
+    cell2.textContent = course.courseNum;
 
     let cell3 = newRow.insertCell(2);
-    cell3.textContent = course.instructor;
+    cell3.textContent = course.courseName;
+
+    let anchor = document.createElement("a");
+    anchor.href = `details.html?courseid=${course.id}`;
+    anchor.textContent = "See details";
+
+    let cell4 = newRow.insertCell(3);
+    cell4.appendChild(anchor);
   }
 }
